3ac9f9a241746c021c125b76ab84769d6a60cbec
Vsevolod Skripnik <vskripnik@bumerang.com>
2023-07-18 20:23:04 +0400

    add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/main.css'],
+  modules: [
+    // pinia plugin
+    '@pinia/nuxt',
+  ],
+
+  imports: {
+    dirs: ['./stores'],
+  },
+
+  pinia: {
+    autoImports: ['defineStore', 'acceptHMRUpdate'],
+  },
 })
