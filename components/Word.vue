1d0941dc2c079d580a261c917d0f4dec07b3a65c
Vsevolod Skripnik <vskripnik@bumerang.com>
2023-07-23 14:12:29 +0400

    add complex store

---
+++ components/Word.vue
@@ -0,0 +1,13 @@
+<template>
+  <span><pre>{{ props.content }} </pre></span>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  content: String,
+})
+</script>
+
+<style></style>
+
+<style scoped></style>
